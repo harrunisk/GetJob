52469847a6d9743d8900163849141b943e02b3a5
mhmtdemir <mistikani.emir@gmail.com>
2018-02-11 17:25:30 +0300

    Anasayfa Ekranı Tasarlandı.ProfilPage yapım aşamasında

--- app/src/main/java/com/example/harun/getjob/UserIntro.java
+++ app/src/main/java/com/example/harun/getjob/UserIntro.java
@@ -1,16 +1,45 @@
 package com.example.harun.getjob;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Harun on 10-Jan-18.
  */
 
 public class UserIntro extends AppCompatActivity{
+
+    Button profile;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        gatherView();
+
+    }
+
+
+
+    public void gatherView(){
+
+
+        profile=(Button)findViewById(R.id.profileButon);
+
+    }
+
+    public void profilPage(View view) {
+
+        Intent intent=new Intent(getApplicationContext(),profilPage.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+
     }
 }
