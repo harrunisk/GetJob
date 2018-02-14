f6d99bffb9907321404571c1cbbfeda284a924d0
mhmtdemir <mistikani.emir@gmail.com>
2018-02-15 00:09:34 +0300

    Birkac bug düzeltildi

---
+++ app/src/main/java/com/example/harun/getjob/Profile/EditProfile.java
@@ -0,0 +1,15 @@
+package com.example.harun.getjob.Profile;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.harun.getjob.R;
+
+public class EditProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_profile);
+    }
+}
